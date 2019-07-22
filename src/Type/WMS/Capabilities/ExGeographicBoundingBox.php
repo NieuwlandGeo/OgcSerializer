8b080e4deb0dbe4bc8bd28c708890ae7d5579f86
allart <allartk@gmail.com>
2019-07-22 09:31:49 +0200

    Add WMS Layer props

---
+++ src/Type/WMS/Capabilities/ExGeographicBoundingBox.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nieuwland\OgcSerializer\Type\WMS\Capabilities;
+
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Base on WMS 1.3.0 EX_GeographicBoundingBox.
+ */
+class ExGeographicBoundingBox
+{
+    /**
+     * @Type("float")
+     *
+     * @var float
+     */
+    private $westBoundLongitude;
+
+    /**
+     * @Type("float")
+     *
+     * @var float
+     */
+    private $eastBoundLongitude;
+
+    /**
+     * @Type("float")
+     *
+     * @var float
+     */
+    private $southBoundLatitude;
+
+    /**
+     * @Type("float")
+     *
+     * @var float
+     */
+    private $northBoundLatitude;
+
+    /**
+     * Get the value of westBoundLongitude.
+     *
+     * @return float
+     */
+    public function getWestBoundLongitude()
+    {
+        return $this->westBoundLongitude;
+    }
+
+    /**
+     * Set the value of westBoundLongitude.
+     *
+     * @param float $westBoundLongitude
+     *
+     * @return self
+     */
+    public function setWestBoundLongitude(float $westBoundLongitude)
+    {
+        $this->westBoundLongitude = $westBoundLongitude;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of eastBoundLongitude.
+     *
+     * @return float
+     */
+    public function getEastBoundLongitude()
+    {
+        return $this->eastBoundLongitude;
+    }
+
+    /**
+     * Set the value of eastBoundLongitude.
+     *
+     * @param float $eastBoundLongitude
+     *
+     * @return self
+     */
+    public function setEastBoundLongitude(float $eastBoundLongitude)
+    {
+        $this->eastBoundLongitude = $eastBoundLongitude;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of southBoundLatitude.
+     *
+     * @return float
+     */
+    public function getSouthBoundLatitude()
+    {
+        return $this->southBoundLatitude;
+    }
+
+    /**
+     * Set the value of southBoundLatitude.
+     *
+     * @param float $southBoundLatitude
+     *
+     * @return self
+     */
+    public function setSouthBoundLatitude(float $southBoundLatitude)
+    {
+        $this->southBoundLatitude = $southBoundLatitude;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of northBoundLatitude.
+     *
+     * @return float
+     */
+    public function getNorthBoundLatitude()
+    {
+        return $this->northBoundLatitude;
+    }
+
+    /**
+     * Set the value of northBoundLatitude.
+     *
+     * @param float $northBoundLatitude
+     *
+     * @return self
+     */
+    public function setNorthBoundLatitude(float $northBoundLatitude)
+    {
+        $this->northBoundLatitude = $northBoundLatitude;
+
+        return $this;
+    }
+}
---
+++ src/Type/WMS/Capabilities/Style.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nieuwland\OgcSerializer\Type\WMS\Capabilities;
+
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Base on WMS 1.3.0 style
+ */
+class Style
+{
+    /**
+     * @Type("string")
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @Type("string")
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Get the value of title
+     *
+     * @return  string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @param  string $title
+     *
+     * @return  self
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return  string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param  string $name
+     *
+     * @return  self
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
