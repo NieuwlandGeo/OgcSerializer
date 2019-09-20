17ff4b973e559ad4fcec8e81d6ec49b85ed8bbd4
Allart Kooiman <akooiman@nieuwland.nl>
2019-09-20 14:47:18 +0200

    test provincie layer

--- tests/Utils/WfsSchemaReaderTest.php
+++ tests/Utils/WfsSchemaReaderTest.php
@@ -23,11 +23,21 @@ class WfsSchemaReaderTest extends TestCase
         $this->wfsSchema = new WfsSchemaReader();
     }
 
-    public function testExtractFieldsFromPdok(): void
+    public function testExtractFieldsFromGemeentenPdok(): void
     {
         $xml    = file_get_contents(FIXTURE_PATH . '/WFS/DescribeFeatureType_geoserver_pdok.xml');
         $fields = $this->wfsSchema->extractFields($xml, 'bestuurlijkegrenzen:gemeenten');
         $this->assertCount(3, $fields);
+        foreach ($fields as $field) {
+            $this->assertInstanceOf(WfsSchemaElement::class, $field);
+        }
+    }
+
+    public function testExtractFieldsFromProvinciePdok(): void
+    {
+        $xml    = file_get_contents(FIXTURE_PATH . '/WFS/DescribeFeatureType_geoserver_pdok.xml');
+        $fields = $this->wfsSchema->extractFields($xml, 'bestuurlijkegrenzen:provincies');
+        $this->assertCount(2, $fields);
         foreach ($fields as $field) {
             $this->assertInstanceOf(WfsSchemaElement::class, $field);
         }
