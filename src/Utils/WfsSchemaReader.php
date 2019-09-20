205ea45dac9572ddcb027ce5ce57793e20320a36
Allart Kooiman <akooiman@nieuwland.nl>
2019-09-20 13:35:00 +0200

    Return array of schemaelements

--- src/Utils/WfsSchemaReader.php
+++ src/Utils/WfsSchemaReader.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Nieuwland\OgcSerializer\Utils;
 
 use DOMNode;
-use Exception;
 use Symfony\Component\DomCrawler\Crawler;
+use function explode;
+use function is_numeric;
 use function sprintf;
+use function strpos;
 use function strstr;
 use function substr;
 
@@ -18,6 +20,10 @@ class WfsSchemaReader
      *
      * @param string $xsd
      * @param string $name featuretype name including prefix
+     *
+     * @throws WfsSchemaException
+     *
+     * @return WfsSchemaElement[]
      */
     public function extractFields(string $xsd, string $name): array
     {
@@ -25,7 +31,7 @@ class WfsSchemaReader
         $crawler    = new Crawler($xsd);
         $schemaNode = $crawler->getNode(0);
         if (! $schemaNode) {
-            throw new Exception('Schema node at position 0 not found');
+            throw new WfsSchemaException('Schema node at position 0 not found');
         }
         $xsdPrefix      = $schemaNode->lookupPrefix('http://www.w3.org/2001/XMLSchema');
         $nameAttr       = $this->getNameAttribute($schemaNode, $name);
@@ -33,7 +39,7 @@ class WfsSchemaReader
         $elementCrawler = $crawler->filterXPath($xpath);
         $elementNode    = $elementCrawler->getNode(0);
         if (! $schemaNode) {
-            throw new Exception(sprintf('Element node with name %s not found', $nameAttr));
+            throw new WfsSchemaException(sprintf('Element node with name %s not found', $nameAttr));
         }
         $complexTypeName       = $elementNode->attributes->getNamedItem('type')->nodeValue;
         $complexNameAttr       = $this->getNameAttribute($schemaNode, $complexTypeName);
@@ -44,11 +50,49 @@ class WfsSchemaReader
             $xsdPrefix
         );
         $complexElementCrawler =  $crawler->filterXPath($complexElementPath);
-
+        $targetNamespace       = $this->getTargetNameSpace($schemaNode);
+        /** @var DOMNode $element */
         foreach ($complexElementCrawler as $element) {
+            $fullType = $this->getAttributeValue($element, 'type');
+            // if type has prefix
+            if (false !== strpos($fullType, ':')) {
+                [$prefix,$typeName] = explode(':', $fullType);
+                $typeNamespaceUri   = $schemaNode->lookupNamespaceUri($prefix);
+            } else {
+                $typeName         = $fullType;
+                $typeNamespaceUri = $targetNamespace;
+            }
+
+            $fields[] = new WfsSchemaElement(
+                $this->getAttributeValue($element, 'name'),
+                $typeName,
+                $typeNamespaceUri,
+                $this->getAttributeValue($element, 'nillable'),
+                $this->getAttributeValue($element, 'minOccurs'),
+                $this->getAttributeValue($element, 'maxOccurs')
+            );
         }
 
         return $fields;
+    }
+
+    private function getAttributeValue(DOMNode $node, string $name)
+    {
+        $attr = $node->attributes->getNamedItem($name);
+        if (! $attr) {
+            return null;
+        }
+        if ('true' === $attr->nodeValue) {
+            return true;
+        }
+        if ('false' === $attr->nodeValue) {
+            return false;
+        }
+        if (is_numeric($attr->nodeValue)) {
+            return (int) $attr->nodeValue;
+        }
+
+        return $attr->nodeValue;
     }
 
     /**
