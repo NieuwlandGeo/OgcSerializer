bc1dca22383347b8bcf6b28a8642bffd8315bae7
Allart Kooiman <akooiman@nieuwland.nl>
2019-11-07 16:43:05 +0100

    use a single service class and a factory per type

--- src/Generic/LayerCapabilitiesInterface.php
+++ src/Generic/LayerCapabilitiesInterface.php
@@ -6,4 +6,26 @@ namespace Nieuwland\OgcSerializer\Generic;
 
 interface LayerCapabilitiesInterface
 {
+    public function getName(): string;
+
+    public function getDefaultCrs(): ?string;
+
+    /**
+     * e.g. png for WMS, geojson for WFS.
+     *
+     * @return string[]|null
+     */
+    public function getDataFormats(): ?array;
+
+    /**
+     * WMS/WMTS.
+     *
+     * @return string[]|null
+     */
+    public function getFeatureInfoFormats(): ?array;
+
+    /**
+     * @return string[]
+     */
+    public function getProjections(): array;
 }
--- src/Generic/ServiceCapabilitiesInterface.php
+++ src/Generic/ServiceCapabilitiesInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Nieuwland\OgcSerializer\Generic;
 
+/**
+ * Holds some common used props for OGC services.
+ */
 interface ServiceCapabilitiesInterface
 {
     public function getTitle(): string;
