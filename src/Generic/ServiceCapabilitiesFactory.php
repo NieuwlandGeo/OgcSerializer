703c53133668be46332e6c54f12ce15d91cdc7a8
Allart Kooiman <akooiman@nieuwland.nl>
2019-11-11 10:34:26 +0100

    WMS & WMTS factory

--- src/Generic/ServiceCapabilitiesFactory.php
+++ src/Generic/ServiceCapabilitiesFactory.php
@@ -4,18 +4,23 @@ declare(strict_types=1);
 
 namespace Nieuwland\OgcSerializer\Generic;
 
+use Nieuwland\OgcSerializer\Type\WFS\Capabilities\v200\Capabilities as WFS2Capabilities;
+use Nieuwland\OgcSerializer\Type\WMS\Capabilities\v130\Capabilities as WMS13Capabilities;
 use Nieuwland\OgcSerializer\Type\WMTS\Capabilities\v10\Capabilities as WMTSCapabilities;
 use RuntimeException;
 
-/**
- * Holds some common used props for OGC services.
- */
 class ServiceCapabilitiesFactory
 {
     public static function create(object $capabilities): ServiceCapabilities
     {
         if ($capabilities instanceof WMTSCapabilities) {
             return self::createFromWMTS($capabilities);
+        }
+        if ($capabilities instanceof WMS13Capabilities) {
+            return self::createFromWMS13($capabilities);
+        }
+        if ($capabilities instanceof WFS2Capabilities) {
+            return self::createFromWFS2($capabilities);
         }
 
         throw new RuntimeException('unknown capabilities type');
@@ -31,5 +36,32 @@ class ServiceCapabilitiesFactory
         }
 
         return new ServiceCapabilities($title, $layers);
+    }
+
+    private static function createFromWMS13(WMS13Capabilities $capabilities): ServiceCapabilities
+    {
+        $title = $capabilities->getService()->getTitle();
+
+        $layers = [];
+        foreach ($capabilities->getLayerNames() as $layername) {
+            $layer = $capabilities->getLayer($layername);
+
+            $layers[] = new LayerCapabilities(
+                $layer->getName(),
+                $layer->getCrsOptions(),
+                null,
+                $capabilities->getCapability()->getRequest()->getGetMap()->getFormat(),
+                $capabilities->getCapability()->getRequest()->getGetFeatureInfo()->getFormat()
+            );
+        }
+
+        return new ServiceCapabilities($title, $layers);
+    }
+
+    private static function createFromWFS2(WFS2Capabilities $capabilities): ServiceCapabilities
+    {
+        $title = $capabilities->getServiceIdentification()->getTitle();
+
+        return new ServiceCapabilities($title, []);
     }
 }
--- tests/Generic/ServiceCapabilitiesFactoryTest.php
+++ tests/Generic/ServiceCapabilitiesFactoryTest.php
@@ -4,25 +4,16 @@ declare(strict_types=1);
 
 namespace Tests\Type\Generic;
 
-use Nieuwland\OgcSerializer\Generic\ServiceCapabilities;
 use Nieuwland\OgcSerializer\Generic\ServiceCapabilitiesFactory;
 use Nieuwland\OgcSerializer\SerializerFactory;
+use Nieuwland\OgcSerializer\Type\WMS\Capabilities\v130\Capabilities as WMS13Capabilities;
 use Nieuwland\OgcSerializer\Type\WMTS\Capabilities\v10\Capabilities;
 use PHPUnit\Framework\TestCase;
 use function file_get_contents;
 
-class WMTSServiceTest extends TestCase
+class ServiceCapabilitiesFactoryTest extends TestCase
 {
-    public function testCanCreateInstance(): void
-    {
-        $xml                 = file_get_contents(FIXTURE_PATH . '/WMTS/wmtsGetCapabilities_response.xml');
-        $serializer          = SerializerFactory::create();
-        $capabilities        = $serializer->deserialize($xml, Capabilities::class, 'xml');
-        $genericCapabilities = ServiceCapabilitiesFactory::create($capabilities);
-        $this->assertInstanceOf(ServiceCapabilities::class, $genericCapabilities);
-    }
-
-    public function testTitle(): void
+    public function testWMTSTitle(): void
     {
         $xml          = file_get_contents(FIXTURE_PATH . '/WMTS/wmtsGetCapabilities_response.xml');
         $serializer   = SerializerFactory::create();
@@ -32,7 +23,7 @@ class WMTSServiceTest extends TestCase
         $this->assertEquals($genericCapabilities->getTitle(), 'Web Map Tile Service');
     }
 
-    public function testLayerNames(): void
+    public function testWMTSLayerNames(): void
     {
         $xml          = file_get_contents(FIXTURE_PATH . '/WMTS/wmtsGetCapabilities_response.xml');
         $serializer   = SerializerFactory::create();
@@ -41,5 +32,28 @@ class WMTSServiceTest extends TestCase
         $genericCapabilities = ServiceCapabilitiesFactory::create($capabilities);
         $this->assertCount(1, $genericCapabilities->getLayerNames());
         $this->assertContains('coastlines', $genericCapabilities->getLayerNames());
+    }
+
+    public function testWMSTitle(): void
+    {
+        $xml          = file_get_contents(FIXTURE_PATH . '/WMS/example_capabilities_sld.xml');
+        $serializer   = SerializerFactory::create();
+        $capabilities = $serializer->deserialize($xml, WMS13Capabilities::class, 'xml');
+        $this->assertInstanceOf(WMS13Capabilities::class, $capabilities);
+        $genericCapabilities = ServiceCapabilitiesFactory::create($capabilities);
+        $this->assertEquals($genericCapabilities->getTitle(), 'Acme Corp. Map Server');
+    }
+
+    public function testWMSLayerNames(): void
+    {
+        $xml                 = file_get_contents(FIXTURE_PATH . '/WMS/example_capabilities_sld.xml');
+        $serializer          = SerializerFactory::create();
+        $capabilities        = $serializer->deserialize($xml, WMS13Capabilities::class, 'xml');
+        $genericCapabilities = ServiceCapabilitiesFactory::create($capabilities);
+        $this->assertCount(8, $genericCapabilities->getLayerNames());
+    }
+
+    public function testWFSTitle(): void
+    {
     }
 }
